968b62192324f3c2c1b8e254612bca75d5386752
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-04-21 16:54:51 +0530

    updated env

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,16 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306, 
-  user: "root",
-  password: "system",
-  database: "school_management",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
 
 pool
   .getConnection()
